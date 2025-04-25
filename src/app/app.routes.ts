ae8f5d7fb51f740a36e06294037275ae35ea3bbf
Alexander Hardtke <5yl3kz@googlemail.com>
2025-04-25 12:07:22 +0200

    general structure for the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LegalNoticeComponent } from './home/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './home/privacy-policy/privacy-policy.component';
+import { StartPageComponent } from './home/start-page/start-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StartPageComponent },
+    { path: 'legal', component: LegalNoticeComponent },
+    { path: 'privacy', component: PrivacyPolicyComponent },
+];
