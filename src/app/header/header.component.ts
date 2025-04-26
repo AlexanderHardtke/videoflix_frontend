50ff8cf08fa025bad4e77dd281dc73509b2d428c
Alexander Hardtke <5yl3kz@googlemail.com>
2025-04-26 12:52:46 +0200

    dark mode switch completed in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,14 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  darkmode: boolean = true;
 
   ngOnInit() {
     switch (localStorage.getItem("color")) {
       case "dark":
-        console.log("dunkel");
+        this.setDarkMode();
         break;
       case "light":
-        console.log("hell");
+        this.setLightMode();
         break;
       default: this.setColorScheme()
         break;
@@ -25,8 +26,29 @@ export class HeaderComponent {
   setColorScheme() {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       localStorage.setItem("color", "dark")
+      this.setDarkMode();
     } else {
       localStorage.setItem("color", "light")
+      this.setLightMode();
+    }
+  }
+
+  setDarkMode() {
+    document.documentElement.classList.add('darkmode');
+    document.documentElement.classList.remove('lightmode');
+  }
+
+  setLightMode() {
+    document.documentElement.classList.add('lightmode');
+    document.documentElement.classList.remove('darkmode');
+  }
+
+  switchColorScheme() {
+    this.darkmode = !this.darkmode;
+    if (this.darkmode) {
+      this.setDarkMode();
+    } else {
+      this.setLightMode();
     }
   }
 
