f9c79b2284456ae4b768d632a06c7e183bfc4b07
Alexander Hardtke <5yl3kz@googlemail.com>
2025-04-28 19:21:37 +0200

    responsive signUp

--- src/app/home/sign-up/sign-up.component.ts
+++ src/app/home/sign-up/sign-up.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-sign-up',
     standalone: true,
-    imports: [],
+    imports: [TranslatePipe, FormsModule, CommonModule],
     templateUrl: './sign-up.component.html',
     styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+    form = {
+        email: "",
+        pw: "",
+        repeatPw: ""
+    }
 
+    registerUser() {
+        console.log(this.form)
+    }
 }
