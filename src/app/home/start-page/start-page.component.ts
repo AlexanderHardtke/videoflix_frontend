73071616d414c03dbfd5a00220f988fb6234d79f
Alexander Hardtke <5yl3kz@googlemail.com>
2025-04-28 16:20:20 +0200

    ngmodel for starting page

--- src/app/home/start-page/start-page.component.ts
+++ src/app/home/start-page/start-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-start-page',
     standalone: true,
-    imports: [TranslatePipe],
+    imports: [TranslatePipe, FormsModule],
     templateUrl: './start-page.component.html',
     styleUrl: './start-page.component.scss'
 })
 export class StartPageComponent {
+    email:string = "";
 
+    moveToRegister(email:string) {
+        console.log(email);
+    }
 }
