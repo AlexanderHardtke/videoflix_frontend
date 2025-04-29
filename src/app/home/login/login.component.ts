de13485fe4b075e582c23d4c3ba7e44d09d7838d
Alexander Hardtke <5yl3kz@googlemail.com>
2025-04-29 17:12:20 +0200

    login started design

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
+import { SVG_PATHS } from '../../assets/img/svg-paths';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [TranslatePipe, FormsModule, CommonModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    passwordType: string = "password";
+    form = {
+        email: "",
+        pw: ""
+    }
 
+    loginUser() { }
+
+    markAsUntouched(item: NgModel) {
+        item.control?.markAsUntouched();
+    }
+
+    onMouseDown(svg: HTMLElement): void {
+        this.passwordType = 'text';
+        const path = svg.querySelector('svg path');
+        if (!path) return;
+        path.setAttribute('d', SVG_PATHS.visible);
+    }
+
+    onMouseUp(svg: HTMLElement): void {
+        this.passwordType = 'password';
+        const path = svg.querySelector('svg path');
+        if (!path) return;
+        path.setAttribute('d', SVG_PATHS.invisible);
+    }
 }
