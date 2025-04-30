a16fdad70222c7c2d2494563c3f82727fba42985
Alexander Hardtke <5yl3kz@googlemail.com>
2025-04-30 08:16:49 +0200

    forgot password created

--- src/app/home/forgot-password/forgot-password.component.ts
+++ src/app/home/forgot-password/forgot-password.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-forgot-password',
     standalone: true,
-    imports: [],
+    imports: [TranslatePipe, FormsModule, CommonModule],
     templateUrl: './forgot-password.component.html',
     styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+    form = {
+        email: ""
+    }
+    
 
+
+    sendEmail() {
+        console.log(this.form.email);
+    }
+
+    markAsUntouched(item: NgModel) {
+        item.control?.markAsUntouched();
+    }
 }
