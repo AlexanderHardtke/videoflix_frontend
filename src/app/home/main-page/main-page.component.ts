da085c53f1faed8a1dade7684be485907818164d
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-02 09:22:15 +0200

    main-page header done

--- src/app/home/main-page/main-page.component.ts
+++ src/app/home/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-main-page',
     standalone: true,
-    imports: [],
+    imports: [TranslatePipe],
     templateUrl: './main-page.component.html',
     styleUrl: './main-page.component.scss'
 })
