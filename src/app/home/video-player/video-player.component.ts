02821d37eeb5562a5ef60ae0c07e41c371f87c67
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-19 21:04:37 +0200

    menu in videos is easing in and out for immersion

--- src/app/home/video-player/video-player.component.ts
+++ src/app/home/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-video-player',
@@ -8,5 +8,68 @@ import { Component } from '@angular/core';
     styleUrl: './video-player.component.scss'
 })
 export class VideoPlayerComponent {
+    @ViewChild('videoRef') videoElement!: ElementRef<HTMLVideoElement>;
+    @ViewChild('header') header!: ElementRef;
+    @Input() videoSrc: string = ''
+    hidden: boolean = false;
+    timer: any = null;
+    margin = 75;
 
+    ngAfterViewInit() {
+        this.resetTimeout();
+    }
+
+    @HostListener('document:mousemove', ['$event'])
+    onMouseMove(event: MouseEvent) {
+        const nearBorder = event.clientY < this.margin || window.innerHeight - event.clientY < this.margin;
+        if ((nearBorder) && this.hidden) this.showHeader();
+        else if (!nearBorder && !this.hidden) this.resetTimeout();
+    }
+
+    @HostListener('document:touchstart', ['$event'])
+    onTouchStart(event: TouchEvent) {
+        this.showHeader();
+    }
+
+    showHeader() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+        if (this.hidden) {
+            const header = this.header.nativeElement;
+            header.style.transform = 'translateY(0)';
+            header.style.opacity = '1';
+            this.hidden = false;
+        }
+        this.resetTimeout();
+    }
+
+    hideHeader() {
+        if (!this.hidden) {
+            const header = this.header.nativeElement;
+            header.style.transform = 'translateY(-100%)';
+            header.style.opacity = '0';
+            this.hidden = true;
+        }
+    }
+    resetTimeout() {
+        if (this.timer) clearTimeout(this.timer);
+        this.timer = setTimeout(() => this.hideHeader(), 3000);
+    }
+
+    ngOnDestroy() {
+        if (this.timer) clearTimeout(this.timer);
+    }
+
+    play() {
+        this.videoElement.nativeElement.play();
+    }
+    pause() {
+        this.videoElement.nativeElement.pause();
+    }
+
+    seekTo(seconds: number) {
+        this.videoElement.nativeElement.currentTime = seconds;
+    }
 }
