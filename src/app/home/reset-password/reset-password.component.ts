73ed8e3f0f705f2003ba3e964910a689463e124c
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-23 19:17:05 +0200

    Reset password page created

--- src/app/home/reset-password/reset-password.component.ts
+++ src/app/home/reset-password/reset-password.component.ts
@@ -1,20 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { SVG_PATHS } from '../../assets/img/svg-paths';
+
 
 @Component({
-    selector: 'app-reset-password',
-    imports: [],
-    templateUrl: './reset-password.component.html',
-    styleUrl: './reset-password.component.scss'
+  selector: 'app-reset-password',
+  imports: [TranslatePipe, FormsModule, CommonModule],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
-    constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+  passwordType: string = "password";
+  repeatPasswordType: string = "password";
+      form = {
+        pw: "",
+        repeatPw: ""
+    }
 
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
-ngOnInit() {
+  ngOnInit() {
     console.log("test");
-    
+
     const token = this.route.snapshot.paramMap.get('token');
     if (token) {
       this.http.post('/api/reset-password/', { token }).subscribe({
@@ -22,5 +33,27 @@ ngOnInit() {
         error: () => this.router.navigate(['/error']),
       });
     }
+  }
+
+  resetPassword() { }
+
+  markAsUntouched(item: NgModel) {
+    item.control?.markAsUntouched();
+  }
+
+  onMouseDown(svg: HTMLElement, pw: string): void {
+    if (pw === 'pw') this.passwordType = 'text';
+    else this.repeatPasswordType = 'text';
+    const path = svg.querySelector('svg path');
+    if (!path) return;
+    path.setAttribute('d', SVG_PATHS.visible);
+  }
+
+  onMouseUp(svg: HTMLElement, pw: string): void {
+    if (pw === 'pw') this.passwordType = 'password';
+    else this.repeatPasswordType = 'password';
+    const path = svg.querySelector('svg path');
+    if (!path) return;
+    path.setAttribute('d', SVG_PATHS.invisible);
   }
 }
