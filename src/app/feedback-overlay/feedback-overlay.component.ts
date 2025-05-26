44b3d3e16e5aee10ae8292599c50ad8f71cea489
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-26 17:34:35 +0200

    created overlay container

--- src/app/feedback-overlay/feedback-overlay.component.ts
+++ src/app/feedback-overlay/feedback-overlay.component.ts
@@ -1,28 +1,28 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-feedback-overlay',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './feedback-overlay.component.html',
   styleUrl: './feedback-overlay.component.scss'
 })
 export class FeedbackOverlayComponent {
-  feedbackText: string | null = null;
-  isActive: boolean = false;
+  // feedbackText: string | null = null;
+  // isActive: boolean = false;
 
-  /**
-  * Displays the feedback message and animates it into view immediately.
-  * @param message - The feedback message to show.
-  */
-  showFeedback(message: string): void {
-    this.feedbackText = message;
-    setTimeout(() => {
-      this.isActive = true;
-    }, 200);
-    this.isActive = false;
-    setTimeout(() => {
-      this.feedbackText = null;
-    }, 1500);
-  }
+  // /**
+  // * Displays the feedback message and animates it into view immediately.
+  // * @param message - The feedback message to show.
+  // */
+  // showFeedback(message: string): void {
+  //   this.feedbackText = "keks";
+  //   setTimeout(() => {
+  //     this.isActive = true;
+  //   }, 200);
+    // this.isActive = false;
+    // setTimeout(() => {
+    //   this.feedbackText = null;
+    // }, 1500);
+  // }
 }
