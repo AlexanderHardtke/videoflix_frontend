67eef402b393220411900caff288b8e1be077f53
Alexander Hardtke <5yl3kz@googlemail.com>
2025-06-06 08:57:44 +0200

    footer to side on main page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-footer',
-    imports: [TranslatePipe, RouterLink],
+    imports: [TranslatePipe, RouterLink, NgClass],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+    currentUrl = '';
 
+    constructor(private router: Router) {
+        this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe((event: NavigationEnd) => {
+                this.currentUrl = event.urlAfterRedirects;
+            });
+    }
 }
