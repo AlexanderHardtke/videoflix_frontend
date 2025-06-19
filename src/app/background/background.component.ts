115da2141829f1719e9482640423dd1aeabf4d15
Alexander Hardtke <5yl3kz@googlemail.com>
2025-06-19 14:35:58 +0200

    fixed background element if not on main-page

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { BackgroundService } from '../services/background.service';
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-background',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss'
 })
 export class BackgroundComponent implements OnInit {
   background = '../assets/img/background.jpg';
+  fixed = true;
 
   constructor(private backgroundService: BackgroundService) { }
 
   ngOnInit() {
-    this.backgroundService.background$.subscribe(url => {
-      this.background = url;
+    this.backgroundService.background$.subscribe(img => {
+      this.background = img;
+      if (img.includes('/assets/img/background')) this.fixed = true;
+      else this.fixed = false;
     });
   }
 
