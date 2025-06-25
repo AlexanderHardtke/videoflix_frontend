69a098dd5579023167db2ce72ed896613f0528ab
Alexander Hardtke <5yl3kz@googlemail.com>
2025-06-25 20:55:00 +0200

    first tessts for footer written

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 import { RouterModule } from '@angular/router';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -29,5 +30,27 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should hide footer on /info', () => {
+    component.currentUrl = '/info';
+    fixture.detectChanges();
+
+    const section = fixture.debugElement.query(By.css('section'));
+    expect(section.nativeElement.classList).toContain('d-none');
+  });
+
+  it('should add class to-side on /main', () => {
+    component.currentUrl = '/main';
+    fixture.detectChanges();
+    const section = fixture.debugElement.query(By.css('section'));
+    expect(section.nativeElement.classList).toContain('to-side');
+  });
+
+  it('should contain links to /privacy and /legal', () => {
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    expect(links.length).toBe(2);
+    expect(links[0].attributes['ng-reflect-router-link']).toBe('/privacy');
+    expect(links[1].attributes['ng-reflect-router-link']).toBe('/legal');
   });
 });
