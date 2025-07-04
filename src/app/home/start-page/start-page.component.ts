7fa25146d6911246ce3690605d21330f78dedb9d
Alexander Hardtke <5yl3kz@googlemail.com>
2025-07-04 16:56:23 +0200

    removed unused services

--- src/app/home/start-page/start-page.component.ts
+++ src/app/home/start-page/start-page.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { RegistrationService } from '../../services/registration.service';
-import { FeedbackService } from '../../services/feedback.service';
 
 @Component({
     selector: 'app-start-page',
